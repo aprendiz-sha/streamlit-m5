ffd60b177953df5371dcfcbf240c6069669ea3a7
aprendiz-sha <smaya020800@gmail.com>
2024-12-18 00:21:47 +0000

    React

--- RetoSMS.py
+++ RetoSMS.py
@@ -28,22 +28,19 @@ estado_civil=st.sidebar.selectbox("Selecciona el estado civil:", df["marital_sta
 
 #Código que permita mostrar un gráfico donde se vislualize la distribución de los puntajes de desempeño
 distr_puntaje = px.histogram(df, x="performance_score")
-#distr_puntaje.show()
 st.plotly_chart(distr_puntaje)
 
 #Código que permita mostrar un gráfico en donde se visualice el promedio de horas trabajadas por el género del empleado
-#horasGenero=df.groupby('gender')["average_work_hours"].sum()
-#graf_horas_genero= px.bar(horasGenero, x='gender', y="average_work_hours")
-#st.plotly_chart(graf_horas_genero)
+horasGenero=df.groupby('gender')["average_work_hours"].mean()
+graf_horas_genero= px.bar(horasGenero, y="average_work_hours", labels={"gender": "Género","average_work_hours": "Horas promedio"})
+st.plotly_chart(graf_horas_genero)
 
 #Código que permita mostrar un gráfico en donde se visualice la edad de los empleados con respecto al salario de los mismos.
-edad_salario= px.scatter(df, x="salary", y="age")
-#edad_salario.show()
+edad_salario= px.scatter(df, y="salary", x="age", labels={"salary":"Salario", "age": "Edad"}, hover_data=["position"], color="gender")
 st.plotly_chart(edad_salario)
 
 #Código que permita mostrar un gráfico en donde se visualice la relación del promedio de horas trabajadas versus el puntaje de desempeño.
-desempeño_horas=px.scatter(df, x="average_work_hours", y="performance_score")
-#desempeño_horas.show()
+desempeño_horas=px.scatter(df, y="average_work_hours", x="performance_score", size="salary")
 st.plotly_chart(desempeño_horas)
 
 #Código que permita desplegar una conclusión sobre el análisis mostrado en la aplicación web
